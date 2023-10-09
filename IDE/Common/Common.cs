0070647317bc1a0c066776c7543201950e4ced96
Cristobal Rivera Moreno <kris_todosinfin@outlook.com>
2023-10-08 21:58:50 -0600

    feat: Dictionary of tokens added.

---
+++ IDE/Common/Common.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Collections.Generic;
+
+namespace IDE.Common
+{
+    public static class Global
+    {
+        public static readonly Dictionary<int,string> tokens = new Dictionary<int, string>(){
+        {3, "id"},
+        {38, "integer"},
+        {37, "real"},
+        {6, "+"},
+        {7, "-"},
+        {8, "*"},
+        {9, "/"},
+        {40, "%"},
+        {11, "<"},
+        {12, "<="},
+        {13, ">"},
+        {14, ">="},
+        {15, "="},
+        {16, "<>"},
+        {17, ":="},
+        {18, "("},
+        {19, ")"},
+        {20, "++"},
+        {21, "--"},
+        {22, ","},
+        {23, ";"},
+        {24, "{"},
+        {25, "}"}
+
+
+        };
+    }
+
+    
+
+
+
+
+}
